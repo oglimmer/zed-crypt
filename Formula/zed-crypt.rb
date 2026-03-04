62c3b9de55725e18f9f115a0d5b871c530262082
Oli Zimpasser <oglimmer@gmail.com>
2026-03-04 22:04:16 +0100

    update zed-crypt to version 0.1.1

--- Formula/zed-crypt.rb
+++ Formula/zed-crypt.rb
@@ -1,7 +1,7 @@
 class ZedCrypt < Formula
   desc "Transparent encryption for Zed editor using ccrypt"
   homepage "https://github.com/oglimmer/zed-crypt"
-  url "https://github.com/oglimmer/zed-crypt/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/oglimmer/zed-crypt/archive/refs/tags/v0.1.1.tar.gz"
   sha256 ""
   license "MIT"
 
